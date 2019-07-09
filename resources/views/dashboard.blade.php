4b84496cb2bac01b4179484864ce989a320f6830
Kacper <kacper@joho.org>
2019-07-09 15:56:18 +0200

    Added placeholder tiles to the dashboard.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,20 +3,35 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+        <div class="col-md-4">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+            <div class="card shadow mb-4 border-bottom-primary">
+                <div class="card-body text-center">
+                    <a href="#" class="btn btn-primary btn-icon-split btn-lg mb-2">
+                        <span class="icon text-white-50">
+                            <i class="fas fa-book"></i>
+                        </span>
+                        <span class="text">{{ __('Grootboekrekeningen') }}</span>
+                    </a>
+                    <p class="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer imperdiet mi nec quam auctor efficitur. Mauris commodo, leo et vulputate maximus, ex sem ultrices odio, eu pretium tortor dui sit amet purus.</p>
                 </div>
             </div>
+
+        </div>
+        <div class="col-md-4">
+
+            <div class="card shadow mb-4 border-bottom-secondary">
+                <div class="card-body text-center">
+                    <a href="#" class="btn btn-secondary btn-icon-split btn-lg mb-2">
+                        <span class="icon text-white-50">
+                            <i class="fas fa-cogs"></i>
+                        </span>
+                        <span class="text">{{ __('Instellingen') }}</span>
+                    </a>
+                    <p class="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer imperdiet mi nec quam auctor efficitur. Mauris commodo, leo et vulputate maximus, ex sem ultrices odio, eu pretium tortor dui sit amet purus.</p>
+                </div>
+            </div>
+
         </div>
     </div>
 </div>
